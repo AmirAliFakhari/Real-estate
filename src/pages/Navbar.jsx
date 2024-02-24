86c8ad566c182135a23adc9f140f7d07f6c405b1
amir z <aafz1382@gmail.com>
2024-02-24 20:48:27 +0330

    navbar-styles-changed

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,13 +1,20 @@
 function Navbar() {
   return (
-    <div className="bg-blue-400 w-[75%] border mt-6 rounded-md m-auto h-2/4 flex flex-row-reverse  items-center">
-      <div className="flex items-center gap-5 ">
-        <li>das</li>
-        <li>dasd</li>
-        <li>adsnj</li>
+    <div className="bg-blue-400 z-50 w-[75%] border mt-6 rounded-md m-auto h-2/4 flex flex-row-reverse justify-between p-2 items-center">
+      <div className="flex items-center gap-5 list-none  ">
+        <div className="flex flex-row-reverse gap-6">
+          <li>اجاره</li>
+          <li>خرید</li>
+          <li>املاک و مستغلات</li>
+          <li>مشاورین املاک</li>
+          <li>اخبار روز</li>
+        </div>
         <img className="w-auto" src="/src/assets/icons/logo.svg" />
       </div>
-      <div className=""></div>
+      <div className="flex items-center justify-center gap-5">
+        <button>ورود</button>
+        <button>ثبت آگهی</button>
+      </div>
     </div>
   );
 }
