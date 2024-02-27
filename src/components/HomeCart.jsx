03f2eafd307ff3fd375118a59cd0cf74d7280840
amir z <aafz1382@gmail.com>
2024-02-27 23:25:23 +0330

    adding-swiper-sliders

--- src/components/HomeCart.jsx
+++ src/components/HomeCart.jsx
@@ -4,10 +4,8 @@ function HomeCart({ title, subTitle, mortgage, monthPrice, homeImg }) {
   //   console.log(title, subTitle, mortgage, monthPrice, homeImg);
   return (
     <>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img className="rounded-t-lg" src={`${homeImg}`} alt="" />
-        </a>
+      <div className="max-w-sm flex flex-col justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <img className="rounded-t-lg w-full" src={`${homeImg}`} alt="" />
         <div className="p-5">
           <a href="#">
             <h5 className="mb-2 font-vazir text-base font-bold tracking-tight text-gray-900 dark:text-white">
@@ -20,7 +18,7 @@ function HomeCart({ title, subTitle, mortgage, monthPrice, homeImg }) {
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {mortgage} | {monthPrice}
           </p>
-          <Link className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <Link className="inline-flex text-red-700 items-center px-3 py-2 text-sm font-medium text-center border border-red-700 rounded-lg hover:bg-blue-red focus:ring-4 focus:outline-none focus:ring-blue-300 ">
             مشاهده
           </Link>
         </div>
