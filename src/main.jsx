192e93a619a8e510b62ecd2097f91b229931eca7
amir z <aafz1382@gmail.com>
2024-03-07 10:16:27 +0330

    creating-redux-slice

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./services/store";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </QueryClientProvider>,
 );
---
+++ src/pages/RegisterHome/registerSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const registerSlice = createSlice({
+    name: "register",
+    initialState: {
+        step: 0
+    },
+    reducers: {
+        handleNext: state => {
+            state.step += 1
+        },
+
+        handlePrev: state => {
+            state.step -= 1
+        }
+    }
+})
+
+export const { handleNext, handlePrev } = registerSlice.actions
+
+export default registerSlice.reducer
