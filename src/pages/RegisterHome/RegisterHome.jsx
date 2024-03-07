404a45bcd0928353dbd2f42918dd5c9f9d475cae
amir z <aafz1382@gmail.com>
2024-03-07 10:06:52 +0330

    redux-addex

--- src/pages/RegisterHome/RegisterHome.jsx
+++ src/pages/RegisterHome/RegisterHome.jsx
@@ -2,38 +2,39 @@ import ButtonDir from "../../components/ButtonDir";
 import Stepper from "awesome-react-stepper";
 import InputForm from "../../components/InputFom";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 function RegiterHome() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    watch,
-  } = useForm();
-  const watchAllFields = watch();
-
-  // function validateStep(step) {
-  //   switch (step) {
-  //     case 1:
-  //       return !!watchAllFields.step1Field;
-  //     case 2:
-  //       return !!watchAllFields.step2Field;
-  //     default:
-  //       return true;
-  //   }
-  // }
+  const { register, handleSubmit } = useForm();
+  const [step, setStep] = useState(0);
 
   const onSubmit = (data) => {
-    console.log(data);
+    switch (step) {
+      case 0:
+        console.log("Step1", data);
+        break;
+      case 1:
+        console.log("step2", data);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleNext = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handleBack = () => {
+    setStep((prevStep) => prevStep - 1);
   };
 
   return (
-    <div className="flex h-screen bg-gray-100 ">
-      <div className="hidden h-screen    sm:flex">
+    <div className="flex h-screen rounded-xl bg-gray-100 sm:rounded-l-3xl ">
+      <div className="hidden h-screen     sm:flex">
         <img
           src="src\assets\imgs\register.svg"
-          className=" max-h-svh w-60 sm:flex sm:object-cover  "
-          alt=""
+          className=" max-h-svh w-60 rounded-l-3xl sm:flex sm:object-cover  "
         />
       </div>
       <div className="flex flex-grow items-center justify-center    ">
@@ -42,18 +43,20 @@ function RegiterHome() {
           fillStroke="#51515153"
           activeColor="#ff0d00"
           onContinue={handleSubmit(onSubmit)}
+          backBtn={<ButtonDir title="back" onClick={handleBack} />}
+          continueBtn={<ButtonDir title="next" onClick={handleNext} />}
+          submitBtn={
+            <ButtonDir title="submit" onClick={handleSubmit(onSubmit)} />
+          }
           stroke={2}
           barWidth="150px"
           contentBoxClassName="stepper__contect"
           progressBarClassName="stepper__bar"
           btnPos="center"
-          backBtn={<ButtonDir title="back" />}
-          continueBtn={<ButtonDir title="next" />}
-          submitBtn={<ButtonDir title="submit" />}
         >
           <form
-            onSubmit={handleSubmit(onsubmit)}
-            className="mx-auto mt-5 max-w-sm   "
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto mt-5 max-w-sm"
           >
             <div className=" flex w-72 flex-col gap-6 sm:w-96 ">
               <InputForm
@@ -86,22 +89,80 @@ function RegiterHome() {
               />
             </div>
           </form>
-          <div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto mt-5 max-w-sm"
+          >
             <div className=" flex w-72 flex-col gap-6 sm:w-96 ">
-              <InputForm title="شهر " type="text" textholder="زواره" />
-              <InputForm title="منطقه" type="text" textholder="بنجیره" />
-              <InputForm title="خیبان اصلی" type="text" textholder="طالقانی" />
-              <InputForm title="خیابان فرعی" type="text" textholder="حشمت" />
+              <div className=" flex w-72 flex-col gap-6 sm:w-96 ">
+                <InputForm
+                  title="شهر"
+                  type="text"
+                  textholder="زواره"
+                  name="city"
+                  register={{ ...register("َcity") }}
+                />
+                <InputForm
+                  title="منطقه"
+                  type="text"
+                  textholder="بنجیره"
+                  name="state"
+                  register={{ ...register("state") }}
+                />
+                <InputForm
+                  title="خیبان اصلی"
+                  type="text"
+                  textholder="طالقانی"
+                  name="main-street"
+                  register={{ ...register("main-street") }}
+                />
+                <InputForm
+                  title="خیابان فرعی"
+                  type="text"
+                  textholder="حشمت"
+                  name="auxiliary-road"
+                  register={{ ...register("auxiliary-road") }}
+                />
+              </div>
             </div>
-          </div>
-          <div>
+          </form>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto mt-5 max-w-sm"
+          >
             <div className=" flex w-72 flex-col gap-6 sm:w-96 ">
-              <InputForm title="شهر " type="text" textholder="زواره" />
-              <InputForm title="منطقه" type="text" textholder="بنجیره" />
-              <InputForm title="خیبان اصلی" type="text" textholder="طالقانی" />
-              <InputForm title="خیابان فرعی" type="text" textholder="حشمت" />
+              <div className=" flex w-72 flex-col gap-6 sm:w-96 ">
+                <InputForm
+                  title="شهر"
+                  type="text"
+                  textholder="زواره"
+                  name="city"
+                  register={{ ...register("َcity") }}
+                />
+                <InputForm
+                  title="منطقه"
+                  type="text"
+                  textholder="بنجیره"
+                  name="state"
+                  register={{ ...register("state") }}
+                />
+                <InputForm
+                  title="خیبان اصلی"
+                  type="text"
+                  textholder="طالقانی"
+                  name="main-street"
+                  register={{ ...register("main-street") }}
+                />
+                <InputForm
+                  title="خیابان فرعی"
+                  type="text"
+                  textholder="حشمت"
+                  name="auxiliary-road"
+                  register={{ ...register("auxiliary-road") }}
+                />
+              </div>
             </div>
-          </div>
+          </form>
         </Stepper>
       </div>
     </div>
