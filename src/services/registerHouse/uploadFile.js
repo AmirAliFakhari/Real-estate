74f2a591ec71fd297c95c5b42769c9f24c611fce
amir z <aafz1382@gmail.com>
2024-03-22 01:32:28 +0330

    fixing file-pic

--- src/services/registerHouse/uploadFile.js
+++ src/services/registerHouse/uploadFile.js
@@ -2,11 +2,14 @@ import supabase from "../supabase";
 
 export default async function uploadFile({ pictures }) {
     console.log(pictures)
+    let random = Math.random()
+    console.log(random)
     const { data, error } = await supabase
         .storage
         .from('apartments')
-        .upload('registerHouse.png', pictures[0], {
+        .upload(`${random}-registerHouse.png`, pictures[0], {
             cacheControl: '3600',
+            upsert: true
         })
 
     if (error) {
