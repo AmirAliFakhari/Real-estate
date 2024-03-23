7641af77918b80402946bffa2f8cd15fa562a1b1
amir z <aafz1382@gmail.com>
2024-03-23 20:03:09 +0330

    adding-useRef-detectClicking

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function DropDown({ title, number }) {
-  const [showDrop, setShowDrop] = useState(true);
+  const [showDrop, setShowDrop] = useState(false);
+  const modalRef = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      console.log(!modalRef.current?.contains(event.target));
+      if (!modalRef.current?.contains(event.target)) {
+        setShowDrop(false);
+      }
+    }
+
+    document.body.addEventListener("click", handleClickOutside);
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <>
+    <div className="flex flex-col">
       <button
+        ref={modalRef}
         onClick={() => setShowDrop((c) => !c)}
         className="flex w-28 items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5  text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 "
         type="button"
@@ -30,7 +46,7 @@ function DropDown({ title, number }) {
 
       <div
         id="dropdown"
-        className={`z-10 ${showDrop ? "hidden" : ""} w-28 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700`}
+        className={`z-10 ${showDrop ? "" : "hidden"} w-28 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700`}
       >
         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
           <li>
@@ -67,7 +83,7 @@ function DropDown({ title, number }) {
           </li>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 
