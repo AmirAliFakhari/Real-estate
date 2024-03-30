2af061c7201c25f1ee8edcf934e014ec72531723
amir z <aafz1382@gmail.com>
2024-03-30 23:07:33 +0330

    styling+handling sorting

--- src/partials/house-ad/Advertisements.jsx
+++ src/partials/house-ad/Advertisements.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import useFilterRegHouse from "../../services/registerHouse/useFilterRegHouse";
 import HomeCart from "../houses/HouseCart";
 import { filterRegHouse } from "../../services/registerHouse/registerHouseAPI";
+import Stepper from "awesome-react-stepper";
 
 function Advertisements({ selectedItems }) {
   const { isLoading, data } = useFilterRegHouse(selectedItems);
@@ -10,7 +11,10 @@ function Advertisements({ selectedItems }) {
     filterRegHouse(selectedItems);
   }, [selectedItems, filterRegHouse]);
 
-  if (isLoading) return <div>salam</div>;
+  if (isLoading) return <Stepper />;
+  else if (!data || data.length === 0)
+    return <div>چنین آگهی وجود نداره (:</div>;
+
   console.log(data);
   return (
     <div className="flex flex-wrap items-baseline justify-center gap-5 sm:justify-start">
