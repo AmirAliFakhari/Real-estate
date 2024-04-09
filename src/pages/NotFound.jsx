53b674a92ad5c79b0cb0f39a129b6e91bc053faa
amir z <aafz1382@gmail.com>
2024-04-09 18:21:28 +0330

    fixing src

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import notFound from "../../assets/imgs/notFound.svg";
+import notFound from "../../src/assets/imgs/notFound.svg";
 
 function NotFound() {
   return (
