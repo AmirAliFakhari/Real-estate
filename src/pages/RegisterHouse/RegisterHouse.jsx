7f974c3db2f9cc7bd2f1a3bee27df348530ac792
amir z <aafz1382@gmail.com>
2024-04-09 18:16:31 +0330

    fixing register src

--- src/pages/RegisterHouse/RegisterHouse.jsx
+++ src/pages/RegisterHouse/RegisterHouse.jsx
@@ -1,11 +1,11 @@
 import StepperForm from "../../components/stepper/StepperForm";
-
+import register from "../../assets/imgs/register.svg";
 function RegisterHouse() {
   return (
     <div className="flex h-screen rounded-xl bg-gray-100 sm:rounded-l-3xl ">
       <div className="hidden h-screen sm:flex">
         <img
-          src="src\assets\imgs\register.svg"
+          src={register}
           className="max-h-svh w-60 rounded-l-3xl sm:flex sm:object-cover"
         />
       </div>
