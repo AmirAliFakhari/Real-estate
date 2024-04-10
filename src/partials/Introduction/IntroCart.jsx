a494beae25b679b541c083edcac2501e59622d4e
amir z <aafz1382@gmail.com>
2024-04-10 12:51:06 +0330

    fixing intro main page

--- src/partials/Introduction/IntroCart.jsx
+++ src/partials/Introduction/IntroCart.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function IntroCart({ img, title, text, buttonText, to }) {
+function IntroCart({ img, title, text, to }) {
   return (
     <div className="flex w-56 flex-col items-center justify-center gap-5 rounded-md  bg-stone-50 p-6 text-center  drop-shadow-2xl md:w-[14rem] lg:w-[20rem]">
       <img className="w-fit" src={img} alt="" />
@@ -12,7 +12,7 @@ function IntroCart({ img, title, text, buttonText, to }) {
         to={to}
         className="w-full rounded-md border bg-red-700 py-2 text-white"
       >
-        {buttonText}
+        {title}
       </Link>
     </div>
   );
--- src/services/data/introductionData.js
+++ src/services/data/introductionData.js
@@ -6,7 +6,7 @@ const introductionData =
             "id": 1,
             "text": "در میان صدها آگهی که روزانه به وب‌سایت سقفینو افزوده می‌شود، با استفاده از بیش از ۲۸ فیلتر کاربردی تلاش کرده‌ایم خانه‌ای که در جست‌وجوی آن هستید را هر چه سریعتر پیدا و اجاره کنید.",
             "buttonText": "اجاره خانه",
-            "title": "به آسانی یک خانه اجاره کنید",
+            "title": "اخبار ",
             "img": "https://ecaeztmdfrcwezajiapg.supabase.co/storage/v1/object/public/aboutIcons/Illustration.svg",
             "link": "register-house",
             "to": "new"
@@ -14,8 +14,7 @@ const introductionData =
         {
             "id": 2,
             "text": "بالای ۱ میلیون آگهی فروش در وب‌سایت سقفینو وجود دارد.\nما علاوه بر آگهی‌های فراوان با به‌کارگیری املاک و مشاورین متخصص در هر شهر، تلاش می‌کنیم در تجربه لذت یک خرید آسان با شما سهیم باشد.",
-            "buttonText": "خرید خانه",
-            "title": "خانه مورد علاقه‌تان را بخرید",
+            "title": "درباره ما",
             "img": "https://ecaeztmdfrcwezajiapg.supabase.co/storage/v1/object/public/aboutIcons/Illustration2.svg",
             "to": "about"
 
@@ -23,8 +22,7 @@ const introductionData =
         {
             "id": 3,
             "text": "آیا می‌دانید میانگین بازدید از وب‌سایت به‌طور متوسط روزانه بالای هزاران نفر است؟\nپس به‌سادگی و با چند کلیک ساده، ملک‌تان را به‌صورت رایگان در سقفینو آگهی و در سریع‌ترین زمان ممکن معامله کنید.",
-            "buttonText": "ثبت آگهی",
-            "title": "مالک هستید؟",
+            "title": "ثبت آگهی",
             "img": "https://ecaeztmdfrcwezajiapg.supabase.co/storage/v1/object/public/aboutIcons/pana.svg",
             "to": "register-house"
 
